8de5e81c5185b2419f49e06405c8e3200c9441bf
VARSHITHA B R <75927392+varsh-21@users.noreply.github.com>
2025-06-23 20:31:01 +0530

    Add files via upload
    Automation signup

---
+++ magento-signup-automation-latest/cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
+  })
+})
